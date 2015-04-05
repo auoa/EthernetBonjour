195edfb9dd2bd786fcecac160efa61ee364b6ce4
auoa <markus@auoa.de>
2015-04-06 00:23:31 +0200

    types changed to be compatible with Arduino IDE 1.6.3

--- utility/EthernetCompat.cpp
+++ utility/EthernetCompat.cpp
@@ -125,7 +125,7 @@ uint16_t ethernet_compat_read_SnRX_RD(int socket)
 
 void ethernet_compat_read_data(int socket, uint8_t* src, uint8_t* dst, uint16_t len)
 {
-   W5100.read_data(socket, src, dst, len);
+   W5100.read_data((uint8_t) socket, (uint16_t) &src, dst, len);
 }
 
 uint8_t ethernet_compat_read_SnSr(int socket)
